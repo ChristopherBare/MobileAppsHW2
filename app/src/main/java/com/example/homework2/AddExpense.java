fd12a0451b4dc0a7b64272a64a88f4f5148f4518
christopher <christopherbare@outlook.com>
2018-07-22 00:53:52 -0400

    Attempt at getting camera/gallery chooser working. Camera permissions broken.

--- app/src/main/java/com/example/homework2/AddExpense.java
+++ app/src/main/java/com/example/homework2/AddExpense.java
@@ -1,9 +1,15 @@
 package com.example.homework2;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,14 +32,25 @@ import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class AddExpense extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 001;
+    private final int PICK_IMAGE_CAMERA = 1, PICK_IMAGE_GALLERY = 2;
+
+    private File destination = null;
+    private InputStream inputStreamImg;
+    private String imgPath = null;
 
     DatabaseReference db_root = FirebaseDatabase.getInstance().getReference();
     FirebaseStorage storage = FirebaseStorage.getInstance();
@@ -41,7 +58,7 @@ public class AddExpense extends AppCompatActivity {
 
     TextView label, id, date;
     EditText name, cost;
-    Button dateButton, imageButton, saveButton;
+    Button dateButton, imageButton, saveButton, btnSelectImage;
     ImageView imageView;
 
     DateFormat dateFormat;
@@ -161,27 +178,98 @@ public class AddExpense extends AppCompatActivity {
 
     // Get photo from camera
     private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            try {
+                PackageManager pm = getPackageManager();
+                int hasPerm = pm.checkPermission(Manifest.permission.CAMERA, getPackageName());
+                if (hasPerm == PackageManager.PERMISSION_GRANTED) {
+                    final CharSequence[] options = {"Take Photo", "Choose From Gallery","Cancel"};
+                    android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(AddExpense.this);
+                    builder.setTitle("Select Option");
+                    builder.setItems(options, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int item) {
+                            if (options[item].equals("Take Photo")) {
+                                dialog.dismiss();
+                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(intent, PICK_IMAGE_CAMERA);
+                            } else if (options[item].equals("Choose From Gallery")) {
+                                dialog.dismiss();
+                                Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                startActivityForResult(pickPhoto, PICK_IMAGE_GALLERY);
+                            } else if (options[item].equals("Cancel")) {
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+                    builder.show();
+                } else
+                    Toast.makeText(this, "Camera Permission error", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(this, "Camera Permission error", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
         }
-    }
 
-    // Get result from getting photo
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            inputStreamImg = null;
+            if (requestCode == PICK_IMAGE_CAMERA) {
+                try {
+                    Uri selectedImage = data.getData();
+                    bitmap = (Bitmap) data.getExtras().get("data");
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
 
-            Bundle extras = data.getExtras();
-            Bitmap bitmap = (Bitmap) extras.get("data");
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    Log.e("Activity", "Pick from Camera::>>> ");
 
-            byteData = baos.toByteArray();
-            imageView.setImageBitmap(bitmap);
+                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                    destination = new File(Environment.getExternalStorageDirectory() + "/" +
+                            getString(R.string.app_name), "IMG_" + timeStamp + ".jpg");
+                    FileOutputStream fo;
+                    try {
+                        destination.createNewFile();
+                        fo = new FileOutputStream(destination);
+                        fo.write(bytes.toByteArray());
+                        fo.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
+                    imgPath = destination.getAbsolutePath();
+                    imageView.setImageBitmap(bitmap);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == PICK_IMAGE_GALLERY) {
+                Uri selectedImage = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
+                    Log.e("Activity", "Pick from Gallery::>>> ");
+
+                    imgPath = getRealPathFromURI(selectedImage);
+                    destination = new File(imgPath.toString());
+                    imageView.setImageBitmap(bitmap);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-    }
+
+        public String getRealPathFromURI(Uri contentUri) {
+            String[] proj = {MediaStore.Audio.Media.DATA};
+            Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+
 
     // ADD TO DATABASE
     private void addExpenseToDb(Expense expense) {
